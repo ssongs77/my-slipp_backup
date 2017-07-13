50981c91434181ee9a596209984a58b3297b8e10
DESKTOP-RQ6DFL3\Administrator <ssongs77@gmail.com>
2017-07-13 11:54:28 +0900

    3-3 수정 변경사항 적용

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -3,28 +3,37 @@ package net.slipp.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
 public class UserController {
 	
 	private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("/user/create")
 	public String create(User user){
 		System.out.println("User : "+ user);
 		users.add(user);
+		userRepository.save(user);
 		
-		return "redirect:/list";
+		return "redirect:/user/list";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("/user/list")
 	public String list(Model model){
 		System.out.println("list");
 		model.addAttribute("users", users);
-		return "list";
+		return "/user/list";
 	}
 }
+ 
