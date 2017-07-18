a665a243ee7a6c50f76a2444d3a018a5782424ce
DESKTOP-RQ6DFL3\Administrator <ssongs77@gmail.com>
2017-07-18 09:52:29 +0900

    7월 18일 commit

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -45,7 +46,14 @@ public class UserController {
 	public String list(Model model){
 		System.out.println("list");
 		model.addAttribute("users", users);
-		return "/user/list";
+		return "/user/list"; 
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateform(@PathVariable Long id, Model model){ // 위에 {id}와 여기 선언한 id 이름이 같아야 함.
+		User user = userRepository.findOne(id);
+		model.addAttribute("user", user);
+		return "/user/updateForm";
 	}
 }
  
