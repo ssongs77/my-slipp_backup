b5fa9ac389118b51c2c65e86cde86a2f3bd2de91
DESKTOP-RQ6DFL3\Administrator <ssongs77@gmail.com>
2017-07-19 14:52:42 +0900

    로그인 구현

--- src/main/java/net/slipp/domain/UserRepository.java
+++ src/main/java/net/slipp/domain/UserRepository.java
@@ -3,5 +3,5 @@ package net.slipp.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	User findByUserId(String userId); //findBy + '조회하고 싶은 속성이름(첫글자 대문자)' (속성) 값을 주면 조회 가능
 }
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -3,6 +3,8 @@ package net.slipp.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,6 +29,24 @@ public class UserController {
 	@GetMapping("/loginForm")
 	public String loginForm(){
 		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session){
+		User user = userRepository.findByUserId(userId);
+		if(user == null)
+		{
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		if(!password.equals(user.getPassword())){
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
 	}
 	
 	@GetMapping("/form")
